b3e01299937f2fee42aa899c0857c745995efa69
Parrikar <parrikaa@pun-parrikaa-m.ds.ad.adp.com>
2016-04-08 11:44:27 +0530

    First commit

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by parrikaa on 08/04/16.
+ */
+import {Component} from 'angular2/core';
+
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular 2 App</h1>'
+})
+
+
+export class AppComponent { }
---
+++ app/main.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by parrikaa on 08/04/16.
+ */
+import {bootstrap} from "angular2/platform/browser";
+import {AppComponent} from "./app.component";
+
+
+
+bootstrap(AppComponent);
